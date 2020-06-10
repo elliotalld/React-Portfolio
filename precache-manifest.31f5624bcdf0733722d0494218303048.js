8853e93ae09b300d16e11d8b469cf0ddf618bec8
root <root@localhost.localdomain>
2020-06-10 15:49:35 -0500

    Updates

---
+++ precache-manifest.31f5624bcdf0733722d0494218303048.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5112cdb06d25a27787193cd1217214",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "30a87167d05b9ad3c45c",
+    "url": "/React-Portfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "c0311fe6bf238d20f0d2",
+    "url": "/React-Portfolio/static/css/main.a550239b.chunk.css"
+  },
+  {
+    "revision": "30a87167d05b9ad3c45c",
+    "url": "/React-Portfolio/static/js/2.08f50858.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/React-Portfolio/static/js/2.08f50858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0311fe6bf238d20f0d2",
+    "url": "/React-Portfolio/static/js/main.e44eac03.chunk.js"
+  },
+  {
+    "revision": "13dc71521cd362f31ac6",
+    "url": "/React-Portfolio/static/js/runtime-main.9f454cb9.js"
+  },
+  {
+    "revision": "da4c70bab72e1c9cd1127a4747e1164e",
+    "url": "/React-Portfolio/static/media/Hamza.da4c70ba.png"
+  },
+  {
+    "revision": "3f9e7dd3f55fc000c89ccfb79e260fc9",
+    "url": "/React-Portfolio/static/media/hamza.3f9e7dd3.jpg"
+  }
+]);
